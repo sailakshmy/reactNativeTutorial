98a9334ed0a04a924ec388eaece07ba9b8abecf0
sailakshmy <sailakshmy94@gmail.com>
2023-10-20 15:13:24 +0530

    Commit-4:- Created a dummy component and moved the previous commit codes into this component

--- myfirstproject/App.js
+++ myfirstproject/App.js
@@ -1,49 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import TrialComponent from './TrialComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text style={{ color: 'red', backgroundColor: 'black', padding: 20, borderColor: 'blue', borderWidth: 2, borderRadius: 50 }}>Open up App.js to start working on your app! Hello!</Text> */}
-      {/* <Text style={{color:'blue', backgroundColor:'black', margin: 20}}>Open up App.js to start working on your app! Hello!</Text> */}
-      {/* <TextInput placeholder='Enter a ' style={{width: 200, backgroundColor:'grey'}} />
-      <TouchableOpacity>
-        <Text>Click Me</Text>
-      </TouchableOpacity> */}
-      {/* <View style={{
-        backgroundColor:'red',
-        width: 80,
-        height: 80,
-        borderRadius: 40
-      }}>
-      </View> */}
-      <View style={styles.container_two}>
-      <View style={{
-        backgroundColor:'red',
-        // width: 80,
-        flex: 1,
-        height: 80,
-        // alignItems: 'center',
-        justifyContent: 'center',
-        // paddingTop: 20,
-        padding: 10
-      }}>
-        <Text>Hi!!!!1</Text>
-      </View>
-      <View style={{
-        backgroundColor: 'blue',
-        // width:80,
-        flex: 2,
-        height: 80,
-        // alignItems: 'center',
-        justifyContent: 'center',
-        // paddingTop: 20
-        padding: 10
-      }}>
-        <Text>HELLLOOOO</Text> 
-      </View>
-      <StatusBar style="auto" />
-    </View>
+     <TrialComponent />
     </View>
   );
 }
@@ -55,7 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  container_two: {
-    flexDirection: 'row-reverse'
-  }
 });
---
+++ myfirstproject/TrialComponent.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+
+export default function TrialComponent() {
+    return (
+    //     {/* <Text style={{ color: 'red', backgroundColor: 'black', padding: 20, borderColor: 'blue', borderWidth: 2, borderRadius: 50 }}>Open up App.js to start working on your app! Hello!</Text> */ }
+    //         {/* <Text style={{color:'blue', backgroundColor:'black', margin: 20}}>Open up App.js to start working on your app! Hello!</Text> */ }
+    // {/* <TextInput placeholder='Enter a ' style={{width: 200, backgroundColor:'grey'}} />
+    //   <TouchableOpacity>
+    //     <Text>Click Me</Text>
+    //   </TouchableOpacity> */}
+    // {/* <View style={{
+    //     backgroundColor:'red',
+    //     width: 80,
+    //     height: 80,
+    //     borderRadius: 40
+    //   }}>
+    //   </View> */}
+    <View style={styles.container_two}>
+        <View style={{
+            backgroundColor: 'red',
+            // width: 80,
+            flex: 1,
+            height: 80,
+            // alignItems: 'center',
+            justifyContent: 'center',
+            // paddingTop: 20,
+            padding: 10
+        }}>
+            <Text>Hi!!!!1</Text>
+        </View>
+        <View style={{
+            backgroundColor: 'blue',
+            // width:80,
+            flex: 2,
+            height: 80,
+            // alignItems: 'center',
+            justifyContent: 'center',
+            // paddingTop: 20
+            padding: 10
+        }}>
+            <Text>HELLLOOOO</Text>
+        </View>
+        <StatusBar style="auto" />
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container_two: {
+        flexDirection: 'row-reverse'
+    }
+});
