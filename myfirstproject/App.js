74b65a22d69d92ad3f9951905eec7d84c4801c74
sailakshmy <sailakshmy94@gmail.com>
2023-10-20 12:46:01 +0530

    Commit-2:- Different React Native Elements

--- myfirstproject/App.js
+++ myfirstproject/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Hello!</Text>
+      <TextInput placeholder='Enter a ' style={{width: 200, backgroundColor:'grey'}} />
+      <TouchableOpacity>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
